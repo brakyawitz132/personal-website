21971fc277737b43ca207dbf7c3325b05fd3053c
Jacob Park <jacobypark122@gmail.com>
2023-11-27 19:16:15 -0500

    creating routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Experience from "./pages/Experience"
+import Projects from "./pages/Projects"
+import NavBar from './components/NavBar';
 
 function App() {
   
 
   return (
-    <div>
-      <div> running</div>
+    <div className='App'>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/projects' element={<Projects />}/>
+          <Route path='/experience' element={<Experience />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
